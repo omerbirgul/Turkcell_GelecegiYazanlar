2a8173f3027094cff14bf90b882781abb2f44a87
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2024-03-14 00:09:21 +0300

    odev-13.03.2024 customer entity added

---
+++ Homeworks/Odev2-13.03.2024/src/main/java/com/turkcell/entities/Customer.java
@@ -0,0 +1,11 @@
+package com.turkcell.entities;
+
+import java.util.Date;
+
+public class Customer {
+    private int id;
+    private  String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+    private  String nationalityId;
+}
